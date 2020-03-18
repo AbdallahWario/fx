3b3280449ec94d4b20701f23ee79bc8c7c1f9030
AbdallahWario <abdallwario96@gmail.com>
2020-03-18 18:09:13 +0300

    Updates

---
+++ precache-manifest.f15b818002c81d44289fc59a3fb3e3b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71ab181d4ee45eef8a29dde9e98ca8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45c78798cbabc44ee34d",
+    "url": "/static/css/main.13788eed.chunk.css"
+  },
+  {
+    "revision": "1f54b5dca495178c3eb5",
+    "url": "/static/js/2.6db6ba0c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.6db6ba0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c78798cbabc44ee34d",
+    "url": "/static/js/main.61badba4.chunk.js"
+  },
+  {
+    "revision": "194a76e4889fb345b939",
+    "url": "/static/js/runtime-main.3babf462.js"
+  },
+  {
+    "revision": "dccbf58a1df0543a34b0c0da6d23245d",
+    "url": "/static/media/forex.dccbf58a.png"
+  }
+]);
